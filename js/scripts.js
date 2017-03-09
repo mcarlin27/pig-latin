3d5ad70c4d55444925d6a2264b28306fc212f4da
Joshua Fairchild and Marilyn Carlin <joshua@>
2017-03-08 19:34:11 -0800

    add business logic to js to begin translation of user input into Pig Latin.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
 // business logic
+var toPigLatin = function(inputString) {
+  return inputString.toLowerCase().split(" ");
 
+
+
+
+
+
+
+
+
+
+}
 
 // front-end logic
 $(function() {
   $("form").submit(function(event){
     var inputString = $("#input").val();
-    // console.log(inputString);
+    console.log(inputString);
     var outputString = toPigLatin(inputString);
+    console.log(outputString);
     $("#output").text(outputString);
 
     event.preventDefault();
