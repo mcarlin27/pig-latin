12481236689d8574837b84e6b3258d920caa407a
Joshua Fairchild and Marilyn Carlin <joshua@>
2017-03-08 20:12:16 -0800

    add js logic to translate any word beginning with a vowel to pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,15 @@
 var toPigLatin = function(inputString) {
   var words = inputString.toLowerCase().split(" ");
   var outputArray = [];
+  var vowels = ["a", "e", "i", "o", "u", "y"]
   words.forEach(function(word) {
     if (!isNaN(parseFloat(word))) {
       outputArray.push(word);
-    };
+    } else if (vowels.includes(word[0]) && word.length === 1) {
+      outputArray.push(word + "yay");
+    } else if (vowels.includes(word[0])) {
+      outputArray.push(word + "ay");
+    }
   });
   console.log(outputArray);
 
